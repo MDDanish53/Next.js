f11ecade5f9f32945c5ec774fa5a0c89d05856a8
Mohammad Danish <12imtiyaz124@gmail.com>
2025-12-01 23:21:59 +0530

    new project init

---
+++ Projects/pixxel/app/page.js
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Mohammad Danish</h1>
+      <Button variant={"primary"}>Click Me!</Button>
+      <Button variant={"glass"}>Click Me!</Button>
+    </div>
+  );
+}
---
+++ Projects/pixxel/hooks/use-parallax.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+const UseParallax = () => {
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => setScrollY(window.scrollY);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  return scrollY;
+};
+
+export default UseParallax;
